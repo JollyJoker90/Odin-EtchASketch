b1ea8adbe3333d825bd319199d50e35a09712e04
JollyJoker90 <eike@LAPTOP-22I97KGM.>
2024-11-24 22:41:39 +0100

    updated new grid settings

--- code.js
+++ code.js
@@ -1,20 +1,29 @@
-const container = document.querySelector(".containerY");
+const frame = document.querySelector(".frame");
+const resetBtn = document.querySelector("#button");
 
 let grid_size = 16;
 
 const createGrid = (size) => {
-    // container.replaceChild();
+    const container = document.createElement("div");
+    container.classList.add("container");
+    frame.append(container);
     for (let i = 0; i < size; i++) {
-        let contX = document.createElement("div");
-        contX.classList.add("containerX")
-        container.append(contX)
+        const gridCol = document.createElement("div");
+        gridCol.classList.add("gridCol");
+        container.append(gridCol);
         for (let j = 0; j < size; j++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
-            contX.append(cell);
+            cell.style.width = `${400 / size}px`;
+            cell.style.height = `${400 / size}px`;
+            gridCol.append(cell);
+            cell.addEventListener("mouseout", () => {
+                setTrail(cell);
+            })
         }
     }
 }
+
 createGrid(grid_size);
 const cells = document.querySelectorAll(".cell");
 
@@ -22,8 +31,11 @@ const setTrail = (cell) => {
     cell.style.backgroundColor = "purple"
 }
 
-cells.forEach(cell => {
-    cell.addEventListener("mouseout", () => {
-        setTrail(cell);
-    })
+
+resetBtn.addEventListener("click", () => {
+    const newGridSize = prompt("test");
+    if (newGridSize < 100) {
+        frame.removeChild(frame.lastChild)
+        createGrid(newGridSize);
+    }
 })
